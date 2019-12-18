8d6a3d02f04ed9f98a7cc86ed649a18ac4b21b9c
Artem <artemis.gorb.14@gmail.com>
2019-12-18 19:28:11 +0300

    First

---
+++ models.py
@@ -0,0 +1,22 @@
+from db import db
+from flask import session
+from datetime import datetime
+from sqlalchemy import func, asc
+import json
+
+
+class Model:
+    errors = []
+
+    def validate(self):
+        self.errors = []
+        return len(self.errors) == 0
+
+    def save(self):
+        if self.validate():
+            if self.id is None:
+                db.session.add(self)
+            db.session.commit()
+            return True
+        else:
+            return False
