ed56d70c103d28f0b89eaa94d2ae4f4c3c4696fa
Artem <artemis.gorb.14@gmail.com>
2019-12-22 14:20:52 +0300

    init

--- models.py
+++ models.py
@@ -29,21 +29,57 @@ class Pupil(db.Model, Model):
     permit = db.Column(db.Integer, unique=True)
     login = db.Column(db.String, unique=True)
     password = db.Column(db.String)
+    school_class = db.Column(db.String)
+
+    def __init__(self, name, surname, clas):
+        self.name = name
+        self.surname = surname
+        self.login = ""
+        self.password = ""
+        self.school_class = clas
+
+    @staticmethod
+    def auth(login, password):
+        return db.session.query(Pupil).filter(Pupil.login == login).filter(Pupil.password == password).first()
 
 
 class Parent(db.Model, Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
     child = db.Column(db.Integer, key=Pupil.id)
     login = db.Column(db.String, unique=True)
     password = db.Column(db.String)
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.login = ""
+        self.password = ""
+
+    @staticmethod
+    def auth(login, password):
+        return db.session.query(Pupil).filter(Pupil.login == login).filter(Pupil.password == password).first()
     
 
 class Teacher(db.Model, Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     position = db.Column(db.String)
     qualification = db.Column(db.String)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
     phone = db.Column(db.Integer)
     email = db.Column(db.Integer)
     login = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     is_admin = db.Column(db.Boolean)
+
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.login = ""
+        self.password = ""
+
+    @staticmethod
+    def auth(login, password):
+        return db.session.query(Pupil).filter(Pupil.login == login).filter(Pupil.password == password).first()
