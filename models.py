6e597af5d79dd8c110bd9182c55c5891fedc1711
Artem <artemis.gorb.14@gmail.com>
2019-12-22 15:13:43 +0300

    1

--- models.py
+++ models.py
@@ -23,13 +23,45 @@ class Model:
             return False
 
 
+class Teacher(db.Model, Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    permit = db.Column(db.Integer, unique=True)
+    position = db.Column(db.String)
+    qualification = db.Column(db.String)
+    name = db.Column(db.String)
+    surname = db.Column(db.String)
+    phone = db.Column(db.Integer)
+    email = db.Column(db.Integer)
+    login = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    is_admin = db.Column(db.Boolean)
+
+    def __init__(self, name, surname, qualification, phone, email, is_admin):
+        self.name = name
+        self.surname = surname
+        self.login = ""
+        self.password = ""
+        self.qualification = qualification
+        self.phone = phone
+        self.email = email
+        self.is_admin = is_admin
+        self.position = "Teacher"
+        self.permit = int(str(self.id) + str(random.randint(1, 1000)))
+
+    @staticmethod
+    def auth(login, password):
+        return db.session.query(Pupil).filter(Pupil.login == login).filter(Pupil.password == password).first()
+
+
 class SchoolClass(db.Model, Model):
     name = db.Column(db.String, primary_key=True, unique=True)
     students_list = db.Column(db.ARRAY)
+    teacher = db.Column(db.Integer, key=Teacher.id)
 
     def __init__(self, name, students_list):
         self.name = name
         self.students_list = list(students_list)
+
 
 class Pupil(db.Model, Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
@@ -74,35 +106,6 @@ class Parent(db.Model, Model):
     @staticmethod
     def auth(login, password):
         return db.session.query(Pupil).filter(Pupil.login == login).filter(Pupil.password == password).first()
-    
 
-class Teacher(db.Model, Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    permit = db.Column(db.Integer, unique=True)
-    position = db.Column(db.String)
-    qualification = db.Column(db.String)
-    name = db.Column(db.String)
-    surname = db.Column(db.String)
-    phone = db.Column(db.Integer)
-    email = db.Column(db.Integer)
-    login = db.Column(db.String, unique=True)
-    password = db.Column(db.String)
-    is_admin = db.Column(db.Boolean)
-
-    def __init__(self, name, surname, qualification, phone, email, is_admin):
-        self.name = name
-        self.surname = surname
-        self.login = ""
-        self.password = ""
-        self.qualification = qualification
-        self.phone = phone
-        self.email = email
-        self.is_admin = is_admin
-        self.position = "Teacher"
-        self.permit = int(str(self.id) + str(random.randint(1, 1000)))
-
-    @staticmethod
-    def auth(login, password):
-        return db.session.query(Pupil).filter(Pupil.login == login).filter(Pupil.password == password).first()
 
 
