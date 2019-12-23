26eb84f602a5de5ea46aedb41f26450bbdc6eb3d
Artem <artemis.gorb.14@gmail.com>
2019-12-23 17:37:51 +0300

    init changes

--- init_database.py
+++ init_database.py
@@ -1,39 +1,39 @@
 from models import Subject
 
 if Subject.get_by_name("Математика") is None:
-    math = Subject("subject", "Математика", "", 1, "1")
+    math = Subject("subject", "Математика", "", 11, "11")
     math.save()
 
 if Subject.get_by_name("Биология") is None:
-    bio = Subject("subject", "Биология", "", 2, "2")
+    bio = Subject("subject", "Биология", "", 1, "1")
 
 if Subject.get_by_name("Химия") is None:
-    bio = Subject("subject", "Химия", "", 3, "3")
+    bio = Subject("subject", "Химия", "", 2, "2")
 
 
 if Subject.get_by_name("Физика") is None:
-    bio = Subject("subject", "Физика", "", 4, "4")
+    bio = Subject("subject", "Физика", "", 3, "3")
 
 if Subject.get_by_name("Русский язык") is None:
-    bio = Subject("subject", "Русский язык", "", 5, "5")
+    bio = Subject("subject", "Русский язык", "", 4, "4")
 
 if Subject.get_by_name("Литература") is None:
-    bio = Subject("subject", "Литература", "", 6, "6")
+    bio = Subject("subject", "Литература", "", 5, "5")
 
 if Subject.get_by_name("История") is None:
-    bio = Subject("subject", "История", "", 7, "7")
+    bio = Subject("subject", "История", "", 6, "6")
 
 if Subject.get_by_name("Информатика") is None:
-    bio = Subject("subject", "Информатика", "", 8, "8")
+    bio = Subject("subject", "Информатика", "", 7, "7")
 
 if Subject.get_by_name("Физ-ра") is None:
-    bio = Subject("subject", "Физ-ра", "", 9, "9")
+    bio = Subject("subject", "Физ-ра", "", 8, "8")
 
 if Subject.get_by_name("Алгебра") is None:
-    bio = Subject("subject", "Алгебра", "", 10, "10")
+    bio = Subject("subject", "Алгебра", "", 9, "9")
 
 if Subject.get_by_name("Геометрия") is None:
-    bio = Subject("subject", "Геометрия", "", 11, "11")
+    bio = Subject("subject", "Геометрия", "", 10, "10")
 
 if Subject.get_by_name("Англ.яз.") is None:
     bio = Subject("subject", "Англ.яз.", "", 12, "12")
