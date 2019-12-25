0d846b4d788d0d00cd09305fb164489b1fa47e16
Artem <artemis.gorb.14@gmail.com>
2019-12-25 16:55:01 +0300

    stupid getter

--- api/timetable.py
+++ api/timetable.py
@@ -3,7 +3,7 @@ from flask import request, session
 from db import db
 import json
 
-from models import Pupil, SchoolClass, Subject
+from models import Pupil, SchoolClass, Subject, Teacher, TimetableDay
 
 
 @app.route("/timetable/timetableClass/<int:id_pupil>", methods=["GET"])
@@ -14,6 +14,86 @@ def get_timetable_pupil(id_pupil):
     timetable = clas.get_timetable_class_list()
 
     return json.dumps(timetable)
+
+
+@app.route("/timetable/timetableClass/teacher", methods=["PUT"])
+def get_timetable_teacher():
+    id_teacher = request.form.get("teacher id")
+    teacher = db.session.query(Teacher).filter(id_teacher == Teacher.id).first()
+    subjects = list(db.session.query(Subject).filther(id_teacher == Subject.teacher_id).all())
+    qual = teacher.qualification
+    schedule = {}
+    for subject in subjects:
+        day1 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_first_lesson).all())
+        for day in day1:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 1
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(1)
+
+        day2 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_second_lesson).all())
+        for day in day2:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 2
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(2)
+
+        day3 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_third_lesson).all())
+        for day in day3:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 3
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(3)
+
+        day4 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_fourth_lesson).all())
+        for day in day4:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 4
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(4)
+
+        day5 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_fifth_lesson).all())
+        for day in day5:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 5
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(5)
+
+        day6 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_sixth_lesson).all())
+        for day in day6:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 6
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(6)
+
+        day7 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_seventh_lesson).all())
+        for day in day7:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 7
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(7)
+
+        day8 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_eighth_lesson).all())
+        for day in day8:
+            day_id = day.id
+            if schedule[f"{day_id % 5}"] is None:
+                schedule[f"{day_id % 5}"] = 8
+            else:
+                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(8)
+
+    return json.dumps(
+        {
+            'teacher id': id_teacher,
+            'timetable': schedule
+        }
+    )
 
 
 @app.route("/timetable/set_homework", methods=["PUT"])
