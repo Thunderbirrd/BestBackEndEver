1797b24488b8ba579391cabb1d5be39e59bd2169
Artem <artemis.gorb.14@gmail.com>
2019-12-25 20:08:57 +0300

    1

--- api/timetable.py
+++ api/timetable.py
@@ -18,74 +18,90 @@ def get_timetable_pupil(id_pupil):
 
 @app.route("/timetable/timetableClass/teacher", methods=["PUT"])
 def get_timetable_teacher():
-    id_teacher = request.form.get("teacher id")
+    id_teacher = request.form.get("teacher_id")
     teacher = db.session.query(Teacher).filter(id_teacher == Teacher.id).first()
-    subjects = list(db.session.query(Subject).filther(id_teacher == Subject.teacher_id).all())
+    subjects = list(db.session.query(Subject).filter(id_teacher == Subject.teacher_id).all())
     schedule = {}
     for subject in subjects:
-        day1 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_first_lesson).all())
+        day1 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_first_lesson).all())
         for day in day1:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 1
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 1
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(1)
+                schedule[s] = list(schedule[s]).append(1)
 
-        day2 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_second_lesson).all())
+        day2 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_second_lesson).all())
         for day in day2:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 2
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 2
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(2)
+                schedule[s] = list(schedule[s]).append(2)
 
-        day3 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_third_lesson).all())
+        day3 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_third_lesson).all())
         for day in day3:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 3
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 3
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(3)
+                schedule[s] = list(schedule[s]).append(3)
 
-        day4 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_fourth_lesson).all())
+        day4 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_fourth_lesson).all())
         for day in day4:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 4
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 4
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(4)
+                schedule[s] = list(schedule[s]).append(4)
 
-        day5 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_fifth_lesson).all())
+        day5 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_fifth_lesson).all())
         for day in day5:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 5
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 5
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(5)
+                schedule[s] = list(schedule[s]).append(5)
 
-        day6 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_sixth_lesson).all())
+        day6 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_sixth_lesson).all())
         for day in day6:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 6
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 6
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(6)
+                schedule[s] = list(schedule[s]).append(6)
 
-        day7 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_seventh_lesson).all())
+        day7 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_seventh_lesson).all())
         for day in day7:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 7
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 7
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(7)
+                schedule[s] = list(schedule[s]).append(7)
 
-        day8 = list(db.session.query(TimetableDay).filther(subject.id == TimetableDay.id_eighth_lesson).all())
+        day8 = list(db.session.query(TimetableDay).filter(subject.id == TimetableDay.id_eighth_lesson).all())
         for day in day8:
             day_id = day.id
-            if schedule[f"{day_id % 5}"] is None:
-                schedule[f"{day_id % 5}"] = 8
+            s = f"{day_id % 5}"
+            a = list(schedule.keys())
+            if a.count(s) == 0:
+                schedule[s] = 8
             else:
-                schedule[f"{day_id % 5}"] = list(schedule[f"{day_id % 5}"]).append(8)
+                schedule[s] = list(schedule[s]).append(8)
 
     return json.dumps(
         {
